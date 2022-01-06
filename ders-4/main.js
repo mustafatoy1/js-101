0db48de512807774d7a3240d5c2f62ad646ab2dc
Mustafa Toy <mustafatoy678@gmail.com>
2022-01-07 01:50:58 +0300

    ders 4 eklendi

---
+++ ders-4/main.js
@@ -0,0 +1,16 @@
+/*
+Coding challenge #3: 
+
+Print the multiplication table with 7
+
+1 * 7 = 7
+2 * 7 = 14
+3 * 7 = 21
+.......
+9 * 7 = 63
+
+*/
+
+for (let index = 1; index < 10; index++) {
+  console.log(index, "* 7 = ", index * 7);
+}
