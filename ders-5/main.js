c38add721ae79b6ebb2b11ed7425b4e9d3f71a60
Mustafa Toy <mustafatoy678@gmail.com>
2022-01-07 23:35:51 +0300

    ders 5

--- ders-5/main.js
+++ ders-5/main.js
@@ -1,2 +1,33 @@
-console.log("ders-5");
-console.log("ders-2");
+/*
+
+1 den 10 a kadar olan sayıları topla ve sonucu ekrana yazdır
+
+1 den 10 a döngü yapacağız
+toplam = 3;
+
+
+toplam = sayac+ toplam
+
+
+
+sayac 1
+sayac 2
+3
+4
+5
+6
+7
+8
+9
+10
+
+
+*/
+
+let toplam = 0;
+
+for (let sayac = 1; sayac <= 10; sayac++) {
+  toplam = sayac + toplam;
+}
+
+console.log(toplam);
