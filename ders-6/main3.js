a5c71cfa53c9af0203b0f16ce1616c7042b44434
Mustafa Toy <mustafatoy678@gmail.com>
2022-01-08 00:29:17 +0300

    ders-6

--- ders-6/main3.js
+++ ders-6/main3.js
@@ -22,4 +22,35 @@ function factorial(sayi) {
 
 const sayi = 19;
 
-factorial(3);
+// factorial(3);
+
+// bir fonksiyon oluştur
+// dışardan gelen isim ve soyisim parametlerini ekrana yazdırsın
+
+function yazdir(isim, soyisim) {
+  console.log(isim, soyisim);
+}
+
+yazdir("yasin", "toy");
+yazdir("mustafa", "toy");
+
+//isim gelecek eğer yasinse ekrana yaz
+
+function isimYazdir(isim, yas) {
+  if (isim === "yasin" || isim === "Yasin" || isim === "YASIN") {
+    console.log("doğru");
+  } else if (isim === "mustafa" && isim === "MUSTAFA") {
+    console.log("Mustafa");
+  } else if (isim === "mustafa" && yas === 25) {
+    console.log("lalalala");
+  } else {
+    console.log("olmadı");
+  }
+}
+
+isimYazdir("Mustafa");
+isimYazdir("yasin");
+isimYazdir("Yasin");
+isimYazdir("MUSTAFA");
+isimYazdir("KASIM");
+isimYazdir("mustafa", 25);
